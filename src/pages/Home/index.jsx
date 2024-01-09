b3e77d9ea027b498a78bb9c372c9a8240e674974
MD Akbar Hossan <89450394+MdAkbar05@users.noreply.github.com>
2024-01-09 17:11:45 +0600

    upload everything

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+export const Home = () => {
+  // const [soccerVideos, setSoccersVideos] = useState([]);
+
+  // useEffect(() => {
+  //   You can fetch api data from anywhere using by useEffect
+  //   fetch("https://www.scorebat.com/video-api/v1")
+  //     .then((response) => response.json())
+  //     .then((data) => setSoccersVideos(data));
+  // }, []);
+
+  return (
+    <>
+      <main> Make Structure of Home page</main>
+    </>
+  );
+};
+
+export default Home;
